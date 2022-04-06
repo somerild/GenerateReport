0dbd13bcb0bb8c914f5d9dc198cdcc6b21e4f0ac
somerild <somerildsolutions@gmail.com>
2022-04-06 11:58:15 -0600

    progress on data array

--- src/fileio/GenerateReport.java
+++ src/fileio/GenerateReport.java
@@ -36,26 +36,28 @@ public class GenerateReport
             try( BufferedReader input = new BufferedReader( new FileReader( fIn ) ) )
             {
                 String record;
-                int state;
+                int[][] data = new int[numberOfRecords][4];
                 int population;
                 int childPopulation;
                 int childPovertyPopulation;
 
                 //print header to System.out
-                System.out.printf("%nFile: " + sourcePath + "%n");
-                System.out.printf("%n%5s %10s %16s %24s %15s%n", "State", "Population", "Child Population", "Child Poverty Population", "% Child Poverty");
-                System.out.printf("----- ---------- ---------------- ------------------------ ---------------%n");
+                //System.out.printf("%nFile: " + sourcePath + "%n");
+                //System.out.printf("%n%5s %10s %16s %24s %15s%n", "State", "Population", "Child Population", "Child Poverty Population", "% Child Poverty");
+                //System.out.printf("----- ---------- ---------------- ------------------------ ---------------%n");
 
-                //loop through records
+                input.readLine(); //omit header
+
+                //loop through records to populate data Array
                 for(int i = 0; i < numberOfRecords; i++)
                 {
                     record = input.readLine();
-                    state = Integer.parseInt(record.substring(0,5).trim());
-                    population = Integer.parseInt(record.substring( 6, 16 ).trim());
-                    childPopulation = Integer.parseInt(record.substring( 17, 33 ).trim());
-                    childPovertyPopulation = Integer.parseInt(record.substring( 34, 58 ).trim());
-
+                    data[i][0] = Integer.parseInt(record.substring(0,5).trim()); //state
+                    data[i][1] = Integer.parseInt(record.substring( 6, 16 ).trim()); //population
+                    data[i][2] = Integer.parseInt(record.substring( 17, 33 ).trim()); //childPopulation
+                    data[i][3] = Integer.parseInt(record.substring( 34, 58 ).trim()); //childPovertyPopulation
                 }
+                System.out.println(data[0][0] + " " + data[0][1] + " " + data[0][2] + " " + data[0][3]);
             }
             catch(IOException e1)
             {
